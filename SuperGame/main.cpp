8f70843f8ff0cf28691a16131965bfa3441af3e6
Tabitha Jarvis <tabitha.jarvis@garmin.com>
2015-03-04 13:38:48 -0600

    Adding header for bitmap files and made MeleeChar a bmp.

--- SuperGame/main.cpp
+++ SuperGame/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
- 
+#include <allegro5/allegro_image.h>
+#include "game_bitmaps.h"
+
 const float FPS = 60;
 const int SCREEN_W = 640;
 const int SCREEN_H = 480;
@@ -9,104 +11,108 @@ enum directional_keys : int {
    KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 };
 enum wasd_keys : int {
-	W_KEY = KEY_UP,
-	A_KEY = KEY_LEFT,
-	S_KEY = KEY_DOWN,
-	D_KEY = KEY_RIGHT
+    W_KEY = KEY_UP,
+    A_KEY = KEY_LEFT,
+    S_KEY = KEY_DOWN,
+    D_KEY = KEY_RIGHT
 };
- 
+
 int main(int argc, char **argv)
 {
    ALLEGRO_DISPLAY *display = NULL;
    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
    ALLEGRO_TIMER *timer = NULL;
-   ALLEGRO_BITMAP *bouncer = NULL;
-   float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
-   float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+   ALLEGRO_BITMAP *melee_char = NULL;
+   float melee_char_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+   float melee_char_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
    bool key[4] = { false, false, false, false };
    bool redraw = true;
    bool doexit = false;
- 
+
    if(!al_init()) {
       fprintf(stderr, "failed to initialize allegro!\n");
       return -1;
    }
- 
+
    if(!al_install_keyboard()) {
       fprintf(stderr, "failed to initialize the keyboard!\n");
       return -1;
    }
- 
+
    timer = al_create_timer(1.0 / FPS);
    if(!timer) {
       fprintf(stderr, "failed to create timer!\n");
       return -1;
    }
- 
+
    display = al_create_display(SCREEN_W, SCREEN_H);
    if(!display) {
       fprintf(stderr, "failed to create display!\n");
       al_destroy_timer(timer);
       return -1;
    }
- 
-   bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-   if(!bouncer) {
+
+   if(!al_init_image_addon()) {
+      fprintf(stderr, "failed to initialize image addon!\n");
+      return -1;
+   }
+
+   melee_char = al_load_bitmap(MELEE_CHAR_BMP);
+
+   if(!melee_char) {
       fprintf(stderr, "failed to create bouncer bitmap!\n");
       al_destroy_display(display);
       al_destroy_timer(timer);
       return -1;
    }
- 
-   al_set_target_bitmap(bouncer);
- 
-   al_clear_to_color(al_map_rgb(255, 0, 255));
- 
+
+   al_convert_mask_to_alpha(melee_char, al_map_rgb(255, 0, 255));
+
    al_set_target_bitmap(al_get_backbuffer(display));
- 
+
    event_queue = al_create_event_queue();
    if(!event_queue) {
       fprintf(stderr, "failed to create event_queue!\n");
-      al_destroy_bitmap(bouncer);
+      al_destroy_bitmap(melee_char);
       al_destroy_display(display);
       al_destroy_timer(timer);
       return -1;
    }
- 
+
    al_register_event_source(event_queue, al_get_display_event_source(display));
- 
+
    al_register_event_source(event_queue, al_get_timer_event_source(timer));
- 
+
    al_register_event_source(event_queue, al_get_keyboard_event_source());
- 
+
    al_clear_to_color(al_map_rgb(0,0,0));
- 
+
    al_flip_display();
- 
+
    al_start_timer(timer);
- 
+
    while(!doexit)
    {
       ALLEGRO_EVENT ev;
       al_wait_for_event(event_queue, &ev);
- 
+
       if(ev.type == ALLEGRO_EVENT_TIMER) {
-         if(key[KEY_UP] && bouncer_y >= 4.0) {
-            bouncer_y -= 4.0;
+         if(key[KEY_UP] && melee_char_y >= 4.0) {
+            melee_char_y -= 4.0;
          }
- 
-         if(key[KEY_DOWN] && bouncer_y <= SCREEN_H - BOUNCER_SIZE - 4.0) {
-            bouncer_y += 4.0;
+
+         if(key[KEY_DOWN] && melee_char_y <= SCREEN_H - BOUNCER_SIZE - 4.0) {
+            melee_char_y += 4.0;
          }
- 
-         if(key[KEY_LEFT] && bouncer_x >= 4.0) {
-            bouncer_x -= 4.0;
+
+         if(key[KEY_LEFT] && melee_char_x >= 4.0) {
+            melee_char_x -= 4.0;
          }
- 
-         if(key[KEY_RIGHT] && bouncer_x <= SCREEN_W - BOUNCER_SIZE - 4.0) {
-            bouncer_x += 4.0;
+
+         if(key[KEY_RIGHT] && melee_char_x <= SCREEN_W - BOUNCER_SIZE - 4.0) {
+            melee_char_x += 4.0;
          }
- 
+
          redraw = true;
       }
       else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
@@ -117,15 +123,15 @@ int main(int argc, char **argv)
             case ALLEGRO_KEY_UP:
                key[KEY_UP] = true;
                break;
- 
+
             case ALLEGRO_KEY_DOWN:
                key[KEY_DOWN] = true;
                break;
- 
-            case ALLEGRO_KEY_LEFT: 
+
+            case ALLEGRO_KEY_LEFT:
                key[KEY_LEFT] = true;
                break;
- 
+
             case ALLEGRO_KEY_RIGHT:
                key[KEY_RIGHT] = true;
                break;
@@ -136,40 +142,40 @@ int main(int argc, char **argv)
             case ALLEGRO_KEY_UP:
                key[KEY_UP] = false;
                break;
- 
+
             case ALLEGRO_KEY_DOWN:
                key[KEY_DOWN] = false;
                break;
- 
-            case ALLEGRO_KEY_LEFT: 
+
+            case ALLEGRO_KEY_LEFT:
                key[KEY_LEFT] = false;
                break;
- 
+
             case ALLEGRO_KEY_RIGHT:
                key[KEY_RIGHT] = false;
                break;
- 
+
             case ALLEGRO_KEY_ESCAPE:
                doexit = true;
                break;
          }
       }
- 
+
       if(redraw && al_is_event_queue_empty(event_queue)) {
          redraw = false;
- 
+
          al_clear_to_color(al_map_rgb(0,0,0));
- 
-         al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
- 
+
+         al_draw_bitmap(melee_char, melee_char_x, melee_char_y, 0);
+
          al_flip_display();
       }
    }
- 
-   al_destroy_bitmap(bouncer);
+
+   al_destroy_bitmap(melee_char);
    al_destroy_timer(timer);
    al_destroy_display(display);
    al_destroy_event_queue(event_queue);
- 
+
    return 0;
 }
